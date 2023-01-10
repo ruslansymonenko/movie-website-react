df38b3426f346968cabb43faf0ba5109704ff14a
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-10 20:58:01 +0200

    created basic website structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import {Header} from './layout/Header.jsx';
+import {Footer} from './layout/Footer.jsx';
+import {Main} from './layout/Main.jsx';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <React.Fragment>
+      <Header/>
+      <Main />
+      <Footer/>
+    </React.Fragment>
   );
 }
 
---
+++ src/layout/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return   <nav className="light-blue lighten-3">
+  <div className="nav-wrapper container">
+    <a href="!#" className="brand-logo">Movies</a>
+    <ul id="nav-mobile" className="right hide-on-med-and-down">
+      <li><a href="!#">Repo</a></li>
+      <li><a href="!#">News</a></li>
+    </ul>
+  </div>
+</nav>
+}
+
+export {Header}
